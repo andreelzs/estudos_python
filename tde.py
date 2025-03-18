eb6b46c0fbc7b0f4d48214a20ca7393dd1205d88
André Ferreira <andreelzs@hotmail.com>
2025-03-17 22:37:33 -0300

    a

--- tde.py
+++ tde.py
@@ -5,6 +5,6 @@ def soma(a, b):
 if __name__ == "__main__":
     num1 = float(input("Digite o primeiro número: "))
     num2 = float(input("Digite o segundo número: "))
-
+aaasdads
     resultado = soma(num1, num2)
     print(f"A soma de {num1} e {num2} é: {resultado}")
